be2bdb4e036705bcd9fe779e8e78a2bc4cedb324
Usuario <Usuario@LAPTOP-PCU1C2D9>
2021-11-09 09:18:13 +0100

    Inicio proyecto

---
+++ Ejer3P1/src/FileManagedment.java
@@ -0,0 +1,13 @@
+
+public class FileManagedment {
+	
+	public static boolean counter(String fileName, char vowel, String fileResultName) {
+		return false;
+		
+	}
+	
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ Ejer3P1/src/FileProperties.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
+public class FileProperties {
+	
+	public static BufferedReader getBufferedReader(String fileName) {
+		return null;
+		
+	}
+
+	public static PrintWriter getPrintWriter(String fileName) {
+		return null;
+		
+	}
+}
