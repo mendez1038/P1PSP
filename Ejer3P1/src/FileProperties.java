7c50259252bbbdfb9996160af092b3ff5d8a8fa7
mendez1038 <dmendez1038@gmail.com>
2021-11-09 10:21:22 +0100

    Clase FileProperties finalizada

--- Ejer3P1/src/FileProperties.java
+++ Ejer3P1/src/FileProperties.java
@@ -1,15 +1,29 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class FileProperties {
-	
+
 	public static BufferedReader getBufferedReader(String fileName) {
-		return null;
-		
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(fileName));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return br;
 	}
 
 	public static PrintWriter getPrintWriter(String fileName) {
-		return null;
-		
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter(new FileWriter(fileName));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return pw;
 	}
 }
